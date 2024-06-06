378220dc91ecbf06879e4e2ae59089f7f1fc5a4d
ManuelSampaio <manuelsampaio1235@gmail.com>
2024-06-06 08:52:55 +0100

    comeco

--- internal/usecases/receberMercadoria/receberMercadoria_test.go
+++ internal/usecases/receberMercadoria/receberMercadoria_test.go
@@ -111,7 +111,7 @@ func TestReceberMercadoria(t *testing.T) {
 	// ao receber : terei o produto, depois irei colocar em uma nota de recebimento,
 	//depois recuperar essa nota (depois de criada, o produto é armazenado em lote)
 	// depois de armazenar no lote guardar em um ficheiro(txt, csv)
-	// pesquisar na arquitetura limpa, onde fica a classe que dados da bd?
+	// pesquisar na arquitetura limpa, onde fica a classe ou entidade que lê os dados da bd ou de um arquivo txt que está na pasta infrastruct?
 
 	// intergarar algumas funcionalidade
 
