8b799fe386ef94a5a1519fea781b7333f2bc166a
ManuelSampaio <manuelsampaio1235@gmail.com>
2024-06-07 11:02:06 +0100

    fazendo cli

--- interno/interface/cli/cli.go
+++ interno/interface/cli/cli.go
@@ -1,2 +1,27 @@
 package cli
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func Cli() {
+	comandoPrincipal := &cobra.Command{Use: "ACME"}
+
+	expedirProduto := &cobra.Command{
+		Use:   "expedirProduto [idProduto] [quantidade] ",
+		Short: "expedir um determinado produto",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Println("Please specify a task to add")
+				return
+			}
+			task := args[0]
+			// Implementar a lógica para adicionar a tarefa aqui
+			fmt.Printf("Adicionando tarefa: %s\n", task)
+		},
+	}
+
+	comandoPrincipal.AddCommand(expedirProduto)
+}
