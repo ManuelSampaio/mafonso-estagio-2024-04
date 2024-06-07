61dc2b3cca00b36c6ec8dc0a1436079a5758f792
ManuelSampaio <manuelsampaio1235@gmail.com>
2024-06-07 11:06:46 +0100

    pasta restaurada

---
+++ interno/casosDeUso/expedirMercadoria/expedirMercadoria.go
@@ -0,0 +1,27 @@
+package expedirMercadoria
+
+import (
+	guiaremessa "ACMELDA/interno/dominio/agregados/guiaRemessa"
+	"ACMELDA/interno/dominio/entidades/lote"
+	guiaRemessaRepositorio "ACMELDA/interno/dominio/repositorio/guiaRemessaRepositorio"
+)
+
+type ExpedirMercadoria struct {
+	lote            lote.Lote
+	guiaRepositorio guiaRemessaRepositorio.GuiaRemessaRepositorio
+}
+
+func (e ExpedirMercadoria) Executa(g *guiaremessa.GuiaRemessa) bool {
+	lotes := e.lote.EncontraProdutoEmUmOuMaisLotes(g.ProdutoID())
+	retirou := e.lote.RetirarProdutoNoLote(lotes, g.Quantidade())
+	guia := e.guiaRepositorio.New()
+
+	if retirou != -1 {
+		guia.CriarGuia(g)
+		if existe, _ := guia.RecuperarGuia(g.Id()); existe.Id() != "" {
+			return true
+		}
+	}
+
+	return false
+}
